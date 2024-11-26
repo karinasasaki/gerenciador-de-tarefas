45b88e20eee679888c94bc53bdf7f4c2c5d1c752
Karina Sasaki <karinasasaki99@gmail.com>
2024-11-26 11:44:37 -0400

    feat: Cria a entidade Tarefa

---
+++ src/main/java/com/karinasasaki/gerenciador_de_tarefas/entities/Tarefa.java
@@ -0,0 +1,76 @@
+package com.karinasasaki.gerenciador_de_tarefas.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+public class Tarefa implements Serializable {
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+  private String titulo;
+  private String descricao;
+  private String status;
+  private Date dataCriacao;
+  private Date dataConclusao;
+
+  public Tarefa() {}
+
+  public Tarefa(Long id, String titulo, String descricao, String status, Date dataCriacao, Date dataConclusao) {
+    this.id = id;
+    this.titulo = titulo;
+    this.descricao = descricao;
+    this.status = status;
+    this.dataCriacao = dataCriacao;
+    this.dataConclusao = dataConclusao;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public Date getDataCriacao() {
+    return dataCriacao;
+  }
+
+  public void setDataCriacao(Date dataCriacao) {
+    this.dataCriacao = dataCriacao;
+  }
+
+  public Date getDataConclusao() {
+    return dataConclusao;
+  }
+
+  public void setDataConclusao(Date dataConclusao) {
+    this.dataConclusao = dataConclusao;
+  }
+}
