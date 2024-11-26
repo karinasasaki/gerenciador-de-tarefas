64536eb2a46f3a144a58967fc6f842de6ae88b4c
Karina Sasaki <karinasasaki99@gmail.com>
2024-11-26 16:13:45 -0400

    feat: Cria service para Tarefa

---
+++ src/main/java/com/karinasasaki/gerenciador_de_tarefas/services/TarefaService.java
@@ -0,0 +1,23 @@
+package com.karinasasaki.gerenciador_de_tarefas.services;
+
+import com.karinasasaki.gerenciador_de_tarefas.entities.Tarefa;
+import com.karinasasaki.gerenciador_de_tarefas.repositories.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TarefaService {
+
+  @Autowired
+  private TarefaRepository tarefaRepository;
+
+  public List<Tarefa> findAll() {
+    return tarefaRepository.findAll();
+  }
+
+  public Tarefa findById(Long id) {
+    return tarefaRepository.findById(id).get();
+  }
+}
