c1c5f054dca501af77b4df3557c6baa0df781522
Karina Sasaki <karinasasaki99@gmail.com>
2024-11-26 12:16:07 -0400

    feat: Configura o banco de dados

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ API para Gerenciamento de Tarefas
 Gerenciador de dependências: Maven
 Linguagem: Java 17
 Framework: Spring Boot 3.4.0
+Banco de dados: H2
+//url: http://localhost:8080/banco-de-dados
--- src/main/java/com/karinasasaki/gerenciador_de_tarefas/entities/Tarefa.java
+++ src/main/java/com/karinasasaki/gerenciador_de_tarefas/entities/Tarefa.java
@@ -1,13 +1,19 @@
 package com.karinasasaki.gerenciador_de_tarefas.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 
+@Entity
+@Table(name = "tarefa")
 public class Tarefa implements Serializable {
   @Serial
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String titulo;
   private String descricao;
