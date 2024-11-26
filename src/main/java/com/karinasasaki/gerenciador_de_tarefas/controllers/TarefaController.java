aa01b94483a4582eebc92a9692233fc3c173f545
Karina Sasaki <karinasasaki99@gmail.com>
2024-11-26 16:14:47 -0400

    feat: Cria endpoint Listar Tarefas

--- src/main/java/com/karinasasaki/gerenciador_de_tarefas/controllers/TarefaController.java
+++ src/main/java/com/karinasasaki/gerenciador_de_tarefas/controllers/TarefaController.java
@@ -1,18 +1,26 @@
 package com.karinasasaki.gerenciador_de_tarefas.controllers;
 
 import com.karinasasaki.gerenciador_de_tarefas.entities.Tarefa;
+import com.karinasasaki.gerenciador_de_tarefas.services.TarefaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/tarefas")
 public class TarefaController {
 
+
+  @Autowired
+  private TarefaService tarefaService;
+
   @GetMapping
-  public ResponseEntity<Tarefa> findAll() {
-    Tarefa tarefa = new Tarefa("Beber água", null);
-    return ResponseEntity.ok().body(tarefa);
+  public ResponseEntity<List<Tarefa>> listarTarefas() {
+    List<Tarefa> tarefas = tarefaService.findAll();
+    return ResponseEntity.ok().body(tarefas);
   }
 }
