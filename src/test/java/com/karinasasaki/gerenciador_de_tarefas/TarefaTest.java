04942960b24adb7c22806953a675ecbdfb03bbfd
Karina Sasaki <karinasasaki99@gmail.com>
2024-11-28 21:14:09 -0400

    test: Adiciona testes para Listar e Excluir tarefa

--- src/test/java/com/karinasasaki/gerenciador_de_tarefas/TarefaTest.java
+++ src/test/java/com/karinasasaki/gerenciador_de_tarefas/TarefaTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -35,5 +36,16 @@ public class TarefaTest {
       tarefaAtualizada = repository.save(tarefaAtualizada);
       System.out.println("Tarefa atualizada: " + tarefaAtualizada);
     }
+  }
+
+  @Test
+  public void deveListarTarefas() {
+    List<Tarefa> tarefas = repository.findAll();
+    tarefas.forEach(System.out::println);
+  }
+
+  @Test
+  public void deveExcluirTarefa() {
+    repository.deleteById(1);
   }
 }
