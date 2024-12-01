ca5aab9ebd79f6cb7a8308c27fc527361aa8f631
Karina Sasaki <karinasasaki99@gmail.com>
2024-12-01 18:06:47 -0400

    fix: Coloca validações em CriarTarefaDTO

--- src/main/java/io/github/karinasasaki/gerenciadordetarefas/controllers/dtos/CriarTarefaDTO.java
+++ src/main/java/io/github/karinasasaki/gerenciadordetarefas/controllers/dtos/CriarTarefaDTO.java
@@ -3,6 +3,8 @@ package io.github.karinasasaki.gerenciadordetarefas.controllers.dtos;
 import io.github.karinasasaki.gerenciadordetarefas.entities.Tarefa;
 import io.github.karinasasaki.gerenciadordetarefas.entities.enums.StatusTarefa;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 import java.util.Calendar;
@@ -10,7 +12,10 @@ import java.util.Calendar;
 @Schema(name = "Tarefa")
 @Builder
 public record CriarTarefaDTO(
+    @NotBlank(message = "O campo titulo deve ser informado")
+    @Size(max = 200, message = "O tamanho do campo titulo excedeu 200 caracteres")
     String titulo,
+    @Size(max = 1500, message = "O tamanho do campo descricao excedeu 1500 caracteres")
     String descricao,
     String status,
     Calendar dataConclusao
