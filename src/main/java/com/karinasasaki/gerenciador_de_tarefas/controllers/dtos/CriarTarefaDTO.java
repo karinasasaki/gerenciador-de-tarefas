b6ede5991b113e89ffb063a3a6e2380032d0fe6f
Karina Sasaki <karinasasaki99@gmail.com>
2024-12-01 03:02:33 -0400

    fix: Possibilita informar status ao criar tarefa

--- src/main/java/com/karinasasaki/gerenciador_de_tarefas/controllers/dtos/CriarTarefaDTO.java
+++ src/main/java/com/karinasasaki/gerenciador_de_tarefas/controllers/dtos/CriarTarefaDTO.java
@@ -1,6 +1,7 @@
 package com.karinasasaki.gerenciador_de_tarefas.controllers.dtos;
 
 import com.karinasasaki.gerenciador_de_tarefas.entities.Tarefa;
+import com.karinasasaki.gerenciador_de_tarefas.entities.enums.StatusTarefa;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
@@ -11,6 +12,7 @@ import java.util.Calendar;
 public record CriarTarefaDTO(
     String titulo,
     String descricao,
+    String status,
     Calendar dataConclusao
 ) {
 
@@ -18,6 +20,8 @@ public record CriarTarefaDTO(
     Tarefa tarefa = new Tarefa();
     tarefa.setTitulo(this.titulo);
     tarefa.setDescricao(this.descricao);
+    if (this.status != null)
+      tarefa.setStatus(StatusTarefa.getStatus(this.status));
     tarefa.setDataConclusao(this.dataConclusao);
     return tarefa;
   }
